9e602948e1e68c64ff6b80f7a740cf09d4bec69c
이선호 <99793526+preferrrr@users.noreply.github.com>
2023-08-29 17:38:54 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,44 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int weight = Integer.parseInt(br.readLine());
 
-        int N = scanner.nextInt();
+        int result = 0;
 
-        int[] greedy = new int[N];
-
-        int count = 0;
-
-        while (true) {
-            if (N == 0) {
-                System.out.println(count);
+        while (weight > 0) {
+            if (weight % 5 == 0) {
+                result += weight / 5;
                 break;
-            } else if (N >= 5) {
-                greedy[count] = 5;
-                N -= 5;
-                count++;
-            } else if (N >= 3 && N < 5) {
-                greedy[count] = 3;
-                N -= 3;
-                count++;
-            }
-            else {
-                boolean temp = false;
-                for(int i = count -1 ; i >= 0; i--) {
-                    if(greedy[i] == 5) {
-                        greedy[i] = 3;
-                        N += 2;
-                        temp = true;
-                        break;
-                    }
-                }
-                if(!temp) {
-                    System.out.println(-1);
-                    break;
-                }
+            } else if(weight >= 3) {
+                weight -= 3;
+                result++;
+            } else {
+                result = -1;
+                break;
             }
         }
 
+        System.out.println(result);
     }
 }
