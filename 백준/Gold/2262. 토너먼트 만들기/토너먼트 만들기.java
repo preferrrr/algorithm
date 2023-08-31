44cd10ab11f91740553887cfa9c90e2ce858dc73
이선호 <99793526+preferrrr@users.noreply.github.com>
2023-08-31 23:53:23 +0900

    [Gold IV] Title: 토너먼트 만들기, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Gold/2262. 토너먼트 만들기/토너먼트 만들기.java
+++ 백준/Gold/2262. 토너먼트 만들기/토너먼트 만들기.java
@@ -2,64 +2,56 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         List<Integer> list = new ArrayList<>();
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             list.add(Integer.parseInt(st.nextToken()));
         }
 
         int result = 0;
 
-        while (list.size() > 1) {
-
-            int min = 0;
-            int minIdx = 0;
-
-            for (int i = 1; i < list.size(); i++) {
-                if(list.get(minIdx) < list.get(i)) {
-                    min = list.get(i);
-                    minIdx = i;
-                }
-            }
-
-            if(minIdx == list.size()-1) {
-                result += Math.abs(list.get(minIdx) - list.get(minIdx - 1));
-                if(list.get(minIdx) > list.get(minIdx - 1))
-                    list.remove(minIdx);
-                else list.remove(minIdx - 1);
-            }
-            else if(minIdx == 0) {
-                result += Math.abs(list.get(minIdx) - list.get(minIdx + 1));
-                if(list.get(minIdx) > list.get(minIdx + 1))
-                    list.remove(minIdx);
-                else list.remove(minIdx + 1);
-            }
-
-            else if(Math.abs(list.get(minIdx) - list.get(minIdx-1)) > Math.abs(list.get(minIdx) - list.get(minIdx + 1))) {
-                result += Math.abs(list.get(minIdx) - list.get(minIdx + 1));
-                if(list.get(minIdx) > list.get(minIdx + 1))
-                    list.remove(minIdx);
-                else list.remove(minIdx + 1);
+        while(list.size() > 1) {
+            int maxIdx = findMax(list);
+            if(maxIdx == 0) {
+                result += list.get(maxIdx) - list.get(1);
+            } else if(maxIdx == list.size() - 1) {
+                result += list.get(maxIdx) - list.get(maxIdx - 1);
             } else {
-                result += Math.abs(list.get(minIdx) - list.get(minIdx - 1));
-                if(list.get(minIdx) > list.get(minIdx - 1))
-                    list.remove(minIdx);
-                else list.remove(minIdx - 1);
+                if(list.get(maxIdx - 1) > list.get(maxIdx + 1))
+                    result += list.get(maxIdx) - list.get(maxIdx - 1);
+                else
+                    result += list.get(maxIdx) - list.get(maxIdx + 1);
             }
+
+            list.remove(maxIdx);
 
         }
 
+
         System.out.println(result);
 
+
+    }
+
+    static int findMax(List<Integer> list) {
+        int maxIdx = 0;
+
+        for(int i = 1 ; i < list.size(); i++) {
+            if(list.get(maxIdx) < list.get(i))
+                maxIdx = i;
+        }
+
+        return maxIdx;
     }
 }
