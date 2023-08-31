f094cc9a1bfcc8b3767842365f1ded86fe3ba1d7
이선호 <99793526+preferrrr@users.noreply.github.com>
2023-08-31 19:27:45 +0900

    [Gold IV] Title: 단어 수학, Time: 124 ms, Memory: 14456 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,70 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
+        //65
+        int[] alp = new int[26];
 
-        List<Alpha> list = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
+        for(int i = 0 ; i < n; i++) {
             String word = br.readLine();
 
-            for (int j = 0; j < word.length(); j++) {
-                String wordTemp = String.valueOf(word.charAt(j));
-                double numTemp = Math.pow(10, word.length()- j - 1);
-
-                int index = find(list, wordTemp);
-
-                if(index != -1) {
-                    list.get(index).value += numTemp;
-                } else {
-                    Alpha alpha = new Alpha();
-                    alpha.key = wordTemp;
-                    alpha.value = (int)numTemp;
-                    list.add(alpha);
-                }
-
+            for(int j = 0; j < word.length(); j++) {
+                alp[word.charAt(j) - 65] += Math.pow(10, word.length() - j - 1);
             }
         }
 
-        Collections.sort(list);
+        Arrays.sort(alp);
 
-        int k = 9;
         int result = 0;
 
-        for(Alpha alpha : list) {
-            result += alpha.value * k;
-            k--;
+        int k = 9;
+        for(int i = 25; i >= 16; i--, k--) {
+            result += alp[i] * k;
         }
 
         System.out.println(result);
-
-
-    }
-
-    public static class Alpha implements Comparable<Alpha> {
-        String key;
-        int value;
-
-        @Override
-        public int compareTo(Alpha o) {
-            return o.value - this.value;
-        }
-    }
-
-    public static int find(List<Alpha> list, String alpha) {
-        for (int i = 0; i < list.size(); i++) {
-            if(list.get(i).key.equals(alpha)) {
-                return i;
-            }
-        }
-
-        return -1;
     }
 }
