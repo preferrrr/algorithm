4dd93747ce14fc980949ae3afabafcfb7385ed93
이선호 <99793526+preferrrr@users.noreply.github.com>
2023-09-01 00:44:28 +0900

    [Gold IV] Title: ABBC, Time: 452 ms, Memory: 29332 KB -BaekjoonHub

--- 백준/Gold/25381. ABBC/ABBC.java
+++ 백준/Gold/25381. ABBC/ABBC.java
@@ -1,45 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
         String word = br.readLine();
 
-        PriorityQueue<Integer> a = new PriorityQueue<>();
-        PriorityQueue<Integer> b = new PriorityQueue<>();
+        PriorityQueue<Integer> A = new PriorityQueue<>();
+        PriorityQueue<Integer> B = new PriorityQueue<>();
 
         int result = 0;
 
-        for(int i = 0 ; i < word.length(); i++) {
-            if(word.charAt(i) == 'A')
-                a.offer(i);
-            else if(word.charAt(i) == 'B')
-                b.offer(i);
-            else if(word.charAt(i)== 'C') {
-                if(!b.isEmpty() && b.peek() < i) {
-                    b.poll();
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == 'A')
+                A.add(i);
+            else if (word.charAt(i) == 'B')
+                B.add(i);
+            else {
+                if(!B.isEmpty() && B.peek() < i) {
                     result++;
+                    B.poll();
                 }
             }
+
         }
 
-        while(!b.isEmpty() && !a.isEmpty()) {
-            if(a.peek() < b.peek()) {
-                a.poll();
-                b.poll();
+        while(!A.isEmpty() && !B.isEmpty()) {
+            if(A.peek() < B.peek()) {
                 result++;
-            } else {
-                b.poll();
-            }
+                A.poll();
+                B.poll();
+            } else B.poll();
         }
-
 
         System.out.println(result);
+
 
     }
 }
