1ea10d714db1bd2b13b5b690bc3ebaa4abb3731e
이선호 <99793526+preferrrr@users.noreply.github.com>
2023-09-02 15:57:19 +0900

    [Gold III] Title: 공주님의 정원, Time: 612 ms, Memory: 54316 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
@@ -1,11 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
@@ -14,15 +14,15 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             Flower flower = new Flower();
 
-            int start = Integer.parseInt(st.nextToken())* 100 + Integer.parseInt(st.nextToken());
-            int end =  Integer.parseInt(st.nextToken())* 100 + Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken()) * 100 + Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken()) * 100 + Integer.parseInt(st.nextToken());
 
             flower.start = start;
             flower.end = end;
 
             list.add(flower);
-        }
 
+        }
 
         Collections.sort(list);
 
@@ -35,42 +35,43 @@ public class Main {
 
         while (startDay < endDay) {
             isFind = false;
+
             for (int i = startIdx; i < n; i++) {
-                if (list.get(i).start > startDay) {
+
+                if (list.get(i).start > startDay)// 현재 꽃의 지는 일보다 늦게 피는 꽃이면 break;
                     break;
-                }
-                if (max < list.get(i).end) {
-                    max = list.get(i).end;
+
+                if (max < list.get(i).end) { // 현재 꽃의 지는 일보다 빨리 피는 꽃 중,
+                    max = list.get(i).end;   // 가장 늦게 지는 꽃 찾기.
                     startIdx = i + 1;
                     isFind = true;
                 }
             }
-            if (isFind) {
+
+            if(isFind) { // 연결되도록 하는 꽃이 있음.
                 startDay = max;
                 count += 1;
-            } else {
+            } else // 끝까지 다 돌았지만 연결되도록 하는 꽃이 없음.
                 break;
-            }
+
         }
-        if (max < endDay) {
+
+        if(max < endDay) // 가장 늦게 지는 꽃의 지는 일이 1130보다 빠름.
             System.out.println(0);
-        } else {
-            System.out.println(count);
-        }
+        else System.out.println(count);
+
 
 
     }
-    public static class Flower implements Comparable<Flower> {
-        int start;
-        int end;
+
+    static class Flower implements Comparable<Flower> {
+        int start, end;
 
         @Override
         public int compareTo(Flower o) {
-            if(this.start == o.start)
-                return -this.end + o.end;
-
+            if (this.start == o.start)
+                return o.end - this.end;
             return this.start - o.start;
-
         }
     }
 }
