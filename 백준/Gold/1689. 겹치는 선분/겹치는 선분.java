174200b9c10e46064465aff22f9ccaaf54616bd3
이선호 <99793526+preferrrr@users.noreply.github.com>
2023-09-05 18:20:36 +0900

    [Gold IV] Title: 겹치는 선분, Time: 2196 ms, Memory: 332860 KB -BaekjoonHub

--- 백준/Gold/1689. 겹치는 선분/겹치는 선분.java
+++ 백준/Gold/1689. 겹치는 선분/겹치는 선분.java
@@ -6,59 +6,58 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
+        int n = Integer.parseInt(br.readLine());
 
         List<Node> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            Node node = new Node(start, end);
-            list.add(node);
 
+            list.add(new Node(start, end));
         }
 
         Collections.sort(list);
 
-        int result = 1;
+        PriorityQueue<Integer> queue = new PriorityQueue();
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        int result = 1;
 
         queue.add(list.get(0).end);
 
-        for(int i = 1 ; i < list.size(); i++) {
-
-            if(!queue.isEmpty() && list.get(i).start < queue.peek()) {
-                queue.add(list.get(i).end);
+        for (int i = 1; i < n; i++) {
+            Node node = list.get(i);
+            if(queue.peek() > node.start) {
+                queue.add(node.end);
                 if(result < queue.size())
                     result = queue.size();
             } else {
-                while(!queue.isEmpty() && list.get(i).start >= queue.peek()) {
+                while(!queue.isEmpty() && queue.peek() <= node.start)
                     queue.poll();
-                }
-                queue.add(list.get(i).end);
+                queue.add(node.end);
             }
-
         }
 
         System.out.println(result);
 
+
     }
 
     static class Node implements Comparable<Node> {
-        int start;
-        int end;
+        int start, end;
 
         public Node(int start, int end) {
             this.start = start;
             this.end = end;
         }
 
+
         @Override
         public int compareTo(Node o) {
-            if (this.start == o.start)
+            if (this.start == o.start) {
                 return this.end - o.end;
+            }
             return this.start - o.start;
         }
     }
