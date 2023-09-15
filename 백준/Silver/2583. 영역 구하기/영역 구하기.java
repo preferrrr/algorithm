ff9583da83bbe9af7e8d48b16e6b2ec2eeb9b6fb
이선호 <99793526+preferrrr@users.noreply.github.com>
2023-09-15 20:30:55 +0900

    [Silver I] Title: 영역 구하기, Time: 156 ms, Memory: 14780 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int[][] arr;
+    static boolean[][] visited;
     static int m, n;
     static int count = 0;
 
@@ -20,6 +18,7 @@ public class Main {
         int k = Integer.parseInt(st.nextToken());
 
         arr = new int[m][n];
+        visited = new boolean[m][n];
 
         for (int t = 0; t < k; t++) {
             st = new StringTokenizer(br.readLine());
@@ -36,13 +35,15 @@ public class Main {
         }
 
         List<Integer> result = new ArrayList<>();
-        for(int i = 0 ; i < m; i++) {
+        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if(arr[i][j] == 0) {
-                    dfs(i,j);
-                    result.add(count);
-                    count = 0;
-                }
+//                if (arr[i][j] == 0) {
+//                    dfs(i, j);
+//                    result.add(count);
+//                    count = 0;
+//                }
+                if (arr[i][j] == 0 && !visited[i][j])
+                    result.add(bfs(i, j));
             }
         }
 
@@ -50,7 +51,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         sb.append(result.size() + "\n");
-        for(int temp : result)
+        for (int temp : result)
             sb.append(temp + " ");
 
         System.out.println(sb);
@@ -59,18 +60,51 @@ public class Main {
 
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
+
     static void dfs(int i, int j) {
         arr[i][j] = -1;
         count++;
 
-        for(int k = 0; k < 4; k++) {
+        for (int k = 0; k < 4; k++) {
             int tempX = i + dx[k];
             int tempY = j + dy[k];
 
-            if(tempX >= 0 && tempX < m && tempY >= 0 && tempY < n && arr[tempX][tempY] == 0) {
+            if (tempX >= 0 && tempX < m && tempY >= 0 && tempY < n && arr[tempX][tempY] == 0) {
                 dfs(tempX, tempY);
             }
         }
+    }
 
+    static int bfs(int i, int j) {
+        Deque<Node> deque = new ArrayDeque<>();
+        deque.add(new Node(i, j));
+        visited[i][j] = true;
+        int count = 0;
+
+        while (!deque.isEmpty()) {
+            Node poll = deque.pollFirst();
+            count++;
+
+            for (int k = 0; k < 4; k++) {
+                int tempX = poll.x + dx[k];
+                int tempY = poll.y + dy[k];
+
+                if (tempX >= 0 && tempX < m && tempY >= 0 && tempY < n && !visited[tempX][tempY] && arr[tempX][tempY] == 0) {
+                    visited[tempX][tempY] = true;
+                    deque.add(new Node(tempX, tempY));
+                }
+            }
+        }
+
+        return count;
+    }
+
+    static class Node {
+        int x, y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
