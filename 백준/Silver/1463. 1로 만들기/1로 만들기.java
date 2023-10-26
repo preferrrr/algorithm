e518dfec09a7c45498af525a32e8b1e2435f5148
이선호 <99793526+preferrrr@users.noreply.github.com>
2023-10-26 14:49:28 +0900

    [Silver III] Title: 1로 만들기, Time: 184 ms, Memory: 38832 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,48 +1,52 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
-    public static void main(String[] args) {
-        //1 : 0 , 2 : 1, 3 : 1 , 4 : 2, 5 : 3, 6 : 2, 7 : 3
-        //8 : 3 , 9 : 2, 10 : 3 ,
-        //12 : 4 2 1
-        //11 : 10으로 만들고 score[10] = 4
-        //11 : -1 -1로 9 만들고 score[9] = 4
-        //
-        //12 : 3으로 나눠지면 1+list[12/4] = 3
-        //12 : 2로 나눠지면 1+list[12/6] = 3
-        //12 : 1+list[11] 5
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        //2로만 나눠짐
-        //14 : 1+list[7] 4
-        //14 : 1+list[13] 5
+        Deque<Node> deque = new ArrayDeque<>();
+        boolean[] visited = new boolean[n + 1];
 
-        //3으로만 나눠짐
-        //15 : 1 + list[5]
-        //15 : 1 + list[14]
+        deque.add(new Node(n, 0));
+        visited[n] = true;
 
+        int result = -1;
 
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        while (!deque.isEmpty()) {
+            Node cur = deque.pollFirst();
+            if(cur.x == 1)
+                result = cur.w;
 
-        int[] count = new int[1000001];
-
-        count[1] = 0;
-        count[2] = 1;
-        count[3] = 1;
-
-        for (int i = 4; i <= n; i++) {
-            if (i % 3 == 0 && i % 2 == 0) {
-                count[i] = Math.min(Math.min(1 + count[i / 3], 1 + count[i / 2]), 1 + count[i - 1]);
-            } else if (i % 3 == 0) {
-                count[i] = Math.min(1 + count[i/3], 1 + count[i-1]);
-            } else if(i % 2 == 0) {
-                count[i] = Math.min(1 + count[i/2], 1 + count[i-1]);
-            } else {
-                count[i] = Math.min(1 + count[i-1], 2 + count[i-2]);
+            if (!visited[cur.x / 3] && cur.x % 3 == 0 && cur.x / 3 > 0) {
+                visited[cur.x / 3] = true;
+                deque.add(new Node(cur.x / 3, cur.w + 1));
             }
 
+            if (!visited[cur.x / 2] && cur.x % 2 == 0 && cur.x / 2 > 0) {
+                visited[cur.x / 2] = true;
+                deque.add(new Node(cur.x / 2, cur.w + 1));
+            }
+
+            if (!visited[cur.x - 1] && cur.x - 1 > 0) {
+                visited[cur.x - 1] = true;
+                deque.add(new Node(cur.x - 1, cur.w + 1));
+            }
         }
 
-        System.out.println(count[n]);
+        System.out.println(result);
+    }
+
+    static class Node {
+        int x, w;
+
+        public Node(int x, int w) {
+            this.x = x;
+            this.w = w;
+        }
     }
 }
