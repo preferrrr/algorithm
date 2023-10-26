290ff8071e18545bd3b615c7499dee30c5f0c392
이선호 <99793526+preferrrr@users.noreply.github.com>
2023-10-26 18:22:57 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,34 +1,32 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
+        for (int test = 0; test < t; test++) {
+            int n = Integer.parseInt(br.readLine());
 
+            int[] dp = new int[n + 1];
 
-        for(int i = 0; i < N; i++) {
-            List<Integer> list = new ArrayList<>();
-            list.add(1);
-            list.add(2);
-            list.add(4);
-            list.add(7);
-            int test = scanner.nextInt();
-
-            for (int j = 4; j < test; j++) {
-                int data = list.get(j - 1) + list.get(j - 2) + list.get(j - 3);
-                list.add(data);
+            dp[1] = 1;
+            if(n >= 2)
+                dp[2] = 2;
+            if(n >= 3)
+                dp[3] = 4;
+            if(n >= 4)
+                dp[4] = 7;
+            for (int i = 5; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
             }
 
-            if(test < 4) {
-                System.out.println(list.get(test-1));
-            } else {
-                System.out.println(list.get(list.size()-1));
-
-            }
+            sb.append(dp[n]).append("\n");
         }
 
+        System.out.print(sb);
     }
 }
