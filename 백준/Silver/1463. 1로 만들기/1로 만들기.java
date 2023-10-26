da698b60b5c5e0a5faff6c200c4399d0c42990b8
이선호 <99793526+preferrrr@users.noreply.github.com>
2023-10-26 17:36:04 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18008 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,32 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
+    static int[] dp;
+    static int x;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int x = Integer.parseInt(br.readLine());
-
-        int[] dp = new int[x + 1];
-        Arrays.fill(dp, 1000001);
+        x = Integer.parseInt(br.readLine());
+        dp = new int[x + 1];
         dp[1] = 0;
 
-        //어떤 숫자가 되는 방법은 2나 3을 곱해서 되거나 1 더하기
-
-        for (int i = 2; i <= x; i++) {
-
-            if(i % 3 == 0 && i / 3 > 0 && dp[i] > dp[i / 3] + 1)
-                dp[i] = dp[i / 3] + 1;
-
-            if(i % 2 == 0 && i / 2 > 0 && dp[i] > dp[i/2] + 1)
-                dp[i] = dp[i / 2] + 1;
-            if(i - 1 > 0 && dp[i] > dp[i-1] + 1)
-                dp[i] = dp[i - 1] + 1;
-
-        }
+        dfs(x);
 
         System.out.println(dp[x]);
+        
+    }
 
+    static int dfs(int n) {
+        if(n == 1 || dp[n] != 0)
+            return dp[n];
+        if (n % 3 == 0 && n % 2 == 0) {
+            return dp[n] = Math.min(dfs(n / 3) + 1, dfs(n / 2) + 1);
+        } else if (n % 3 == 0) {
+            return dp[n] = Math.min(dfs(n / 3) + 1, dfs(n - 1) + 1);
+        } else if (n % 2 == 0) {
+            return dp[n] = Math.min(dfs(n / 2) + 1, dfs(n - 1) + 1);
+        } else
+            return dp[n] = dfs(n - 1) + 1;
     }
 }
