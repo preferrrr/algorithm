fc73eab65f1d22eef8418e255381dbd51e42fb7f
이선호 <99793526+preferrrr@users.noreply.github.com>
2023-10-27 17:43:25 +0900

    [Silver II] Title: 연속합, Time: 264 ms, Memory: 23840 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        int max = arr[0];
+        dp[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+
+            //현재까지 연속된값 vs 현재부터 연속된 값
+
+            dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
+
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+
+    }
+}
