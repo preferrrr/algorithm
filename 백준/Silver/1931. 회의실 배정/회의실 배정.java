9f95606fbfe1e6b333d5065964dce3de287e46ca
이선호 <99793526+preferrrr@users.noreply.github.com>
2023-11-08 21:43:38 +0900

    [Silver I] Title: 회의실 배정, Time: 476 ms, Memory: 41608 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,57 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    public static class Meeting {
-        int start;
-        int end;
-
-        public Meeting(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-    }
-
-    public static Comparator<Meeting> comparator = new Comparator<Meeting>() {
-        @Override
-        public int compare(Meeting o1, Meeting o2) {
-            if(o1.end == o2.end) {
-                return o1.start - o2.start;
-            }
-            return o1.end - o2.end;
-        }
-    };
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
-        Meeting[] meetings = new Meeting[n];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            Meeting meeting = new Meeting(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            meetings[i] = meeting;
+        PriorityQueue<Meeting> queue = new PriorityQueue<>();
+        for(int i = 0 ; i < n; i++) {
+            StringTokenizer st= new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            queue.add(new Meeting(start, end));
         }
-        Arrays.sort(meetings, comparator);
 
-        int end = meetings[0].end;
         int count = 0;
+        int end = 0;
+        while(!queue.isEmpty()) {
+            Meeting poll = queue.poll();
 
-        for(int i = 1 ; i < n; i++) {
-            if(meetings[i].start >= end) {
-                end = meetings[i].end;
+            if(end <= poll.start) {
+                end = poll.end;
                 count++;
             }
         }
 
-
-        System.out.println(count + 1);
-
+        System.out.println(count);
 
 
+    }
+
+    static class Meeting implements Comparable<Meeting> {
+        int start, end;
+        public Meeting(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public int compareTo(Meeting o) {
+            if(this.end == o.end) {
+                return this.start - o.start;
+            }
+            return this.end - o.end;
+        }
     }
 }
