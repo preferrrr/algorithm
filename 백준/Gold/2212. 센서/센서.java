4c2de33dfaf0a8b87d880f16d6a5673f94a58ec4
이선호 <99793526+preferrrr@users.noreply.github.com>
2024-01-30 15:47:07 +0900

    [Gold V] Title: 센서, Time: 196 ms, Memory: 16240 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -1,42 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int n, k;
+    static int[] arr;
+    static int[] sub;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(solve());
+    }
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        int k = Integer.parseInt(br.readLine());
-
+    static void input() throws IOException {
+        n = Integer.parseInt(br.readLine());
+        k = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        sub = new int[n - 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] sensor = new int[n];
 
         for (int i = 0; i < n; i++) {
-            sensor[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(sensor);
-
-        int[] sub = new int[n - 1];
+        Arrays.sort(arr);
 
         for (int i = 0; i < n - 1; i++) {
-            sub[i] = sensor[i + 1] - sensor[i];
+            sub[i] = arr[i + 1] - arr[i];
         }
 
         Arrays.sort(sub);
+    }
 
+    static int solve() {
         int result = 0;
 
-        for (int i = 0; i < n - k; i++) {
+        for (int i = 0; i < n - k ; i++) {
             result += sub[i];
         }
 
-        System.out.println(result);
-
+        return result;
     }
 }
