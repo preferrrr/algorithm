f729d6b6425daf4bfe94fa27a8f633195dd27827
이선호 <99793526+preferrrr@users.noreply.github.com>
2024-02-02 00:48:02 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1336 ms, Memory: 123076 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,19 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     static int n, m;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int[][] arr, dp;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static List<Node> nodes = new ArrayList<>();
 
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.print(solve());
+    }
+
+    static void input() throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
         arr = new int[n + 1][n + 1];
         dp = new int[n + 1][n + 1];
 
@@ -25,27 +32,42 @@ public class Main {
             }
         }
 
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            nodes.add(new Node(Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken())));
+        }
 
+    }
+
+    static String solve() {
         StringBuilder sb = new StringBuilder();
 
         for (int t = 0; t < m; t++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken());
-
-            //int sum = dp[a][d] + dp[c][d] - dp[a][b - 1] - dp[c][b - 1];
+            Node node = nodes.get(t);
 
             int result = 0;
-
-            for (int i = a; i <= c; i++) {
-                result += dp[i][d] - dp[i][b-1];
+            for (int i = node.x1; i <= node.x2; i++) {
+                result += dp[i][node.y2] - dp[i][node.y1 - 1];
             }
+
             sb.append(result).append("\n");
-
         }
-        System.out.print(sb);
 
+        return sb.toString();
+    }
+
+
+    static class Node {
+        int x1, y1, x2, y2;
+
+        public Node(int x1, int y1, int x2, int y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
     }
 }
