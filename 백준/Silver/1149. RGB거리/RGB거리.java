33020d67efe6e0ca42c549aee59f37f158c747a2
이선호 <99793526+preferrrr@users.noreply.github.com>
2024-02-05 21:39:44 +0900

    [Silver I] Title: RGB거리, Time: 140 ms, Memory: 14644 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,63 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int n;
     static int[][] arr, dp;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+    static void input() throws IOException {
+        n =  Integer.parseInt(br.readLine());
         arr = new int[n][3];
         dp = new int[n][3];
+
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 3; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+            int r = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            arr[i][0] = r;
+            arr[i][1] = g;
+            arr[i][2] = b;
         }
 
-        // 0, 1, 2 각각을 시작으로.
-
-        int result = Integer.MAX_VALUE;
-
-        for (int i = 0; i < 3; i++) {
-            dfs(0, i, 0);
-            for (int j = 0; j < 3; j++) {
-                if (result > dp[n - 1][j])
-                    result = dp[n - 1][j];
-            }
-
+        dp[0][0] = arr[0][0];
+        dp[0][1] = arr[0][1];
+        dp[0][2] = arr[0][2];
+    }
+    
+    static int solve() {
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
         }
-
-        System.out.println(result);
-
+        return Math.min(dp[n - 1][0], Math.min(dp[n - 1][1], dp[n - 1][2]));
     }
 
-
-    static void dfs(int x, int y, int before) {
-
-        if (dp[x][y] == 0 || dp[x][y] > before + arr[x][y]) {
-            dp[x][y] = arr[x][y] + before;
-        }
-        else return;
-
-        if (x == n - 1)
-            return;
-
-        if (y == 0) {
-            dfs(x + 1, 1, dp[x][y]);
-            dfs(x + 1, 2, dp[x][y]);
-        } else if (y == 1) {
-            dfs(x + 1, 0, dp[x][y]);
-            dfs(x + 1, 2, dp[x][y]);
-        } else {
-            dfs(x + 1, 0, dp[x][y]);
-            dfs(x + 1, 1, dp[x][y]);
-        }
-
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(solve());
     }
+
 }
+
