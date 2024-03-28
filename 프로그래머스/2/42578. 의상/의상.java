ee4e05f8b7200863747fb14bd90ac2f6754a1983
이선호 <99793526+preferrrr@users.noreply.github.com>
2024-03-28 16:01:55 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -6,29 +6,18 @@ class Solution {
         int answer = 1;
         
         Map<String, List<String>> map = new HashMap<>();
-        List<String> kind = new ArrayList<>();
         
         for(int i = 0 ; i < clothes.length; i++) {
             if(!map.containsKey(clothes[i][1])) {
-                
-                List<String> temp = new ArrayList<>();
-                temp.add(clothes[i][0]);
-                
-                map.put(clothes[i][1], temp);
-                
-                kind.add(clothes[i][1]);
-
-            } else {
-                map.get(clothes[i][1]).add(clothes[i][0]);
+                map.put(clothes[i][1], new ArrayList<>());
             }
+            
+            map.get(clothes[i][1]).add(clothes[i][0]);
         }
         
-        for(int i = 0 ; i < kind.size(); i++) {
-            answer *= map.get(kind.get(i)).size() + 1;
-
+        for (String kind : map.keySet()) {
+            answer *= map.get(kind).size() + 1;
         }
-        
-
         
         return answer - 1;
     }
