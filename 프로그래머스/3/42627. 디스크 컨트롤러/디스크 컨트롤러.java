f2b21e3c8fd30dbbe20cbe1f1ae31245fcdba1b3
이선호 <99793526+preferrrr@users.noreply.github.com>
2024-03-29 15:22:48 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 10.53 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,70 +2,46 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-    
-        PriorityQueue<Node> job = new PriorityQueue<>((o1, o2)-> o1.x - o2.x);
         
-        for(int i = 0 ; i < jobs.length; i++) {
-            job.add(new Node(jobs[i][0], jobs[i][1]));
-        }
+        PriorityQueue<Node> wait = new PriorityQueue<>((o1, o2) -> o1.requestTime - o2.requestTime); // 큐에서 각 작업은 작업에 걸리는 시간 순으로 정렬됨.
+        PriorityQueue<Node> ready = new PriorityQueue<>((o1, o2)-> o1.processingTime - o2.processingTime); //큐에서 각 작업은 시작 시간 순으로 정렬됨.
         
-        PriorityQueue<Node> queue = new PriorityQueue<>((o1, o2) -> o1.y - o2.y);
+        for(int i = 0 ; i < jobs.length; i++) 
+            wait.add(new Node(jobs[i][0], jobs[i][1]));
                 
-        int waitTime = 0;
-        int curTime = 0;
-        int temp = job.peek().x;
-        
-        int processTime = 0;
-        int endTime = 0;
-        
-        boolean isWorking = false;
-        
+        int waitTime = 0; //총 기다린 시간.
+        int curTime = 0; //현재 시간
         
         while (true) {
            
-            while (!job.isEmpty() && job.peek().x <= curTime) { //현재 시간에 들어온 요청 있으면 큐에 넣어.
-                queue.add(job.poll());
-                System.out.println("curTime: " + curTime + ", queue.size: " + queue.size() );
+            while (!wait.isEmpty() && wait.peek().requestTime <= curTime) { //현재 시간 이하에 요청된 작업들이 있으면, 작업 대기 큐에 넣음.
+                ready.add(wait.poll());
             }
             
-            if (isWorking) { //현재 작업 중
-                processTime++;
-                
-                if(processTime == endTime) {
-                    processTime = 0;
-                    endTime = 0;
-                    isWorking = false;
-                }
-            }
             
-            if (!isWorking) { 
-                if (!queue.isEmpty()) {
-                    Node poll = queue.poll();
-                    waitTime += curTime - poll.x;
-                    System.out.println("curTime: " +curTime + ", poll.x" + poll.x);
-                    endTime = poll.y;
-                    isWorking = true;
-                } else if(queue.isEmpty() && job.isEmpty())
-                    break;
+            if (ready.isEmpty() && !wait.isEmpty()) { //현재 작업 중인게 없고, 처리해야할 작업은 남은 경우
+                waitTime -= wait.peek().requestTime - curTime;
+                curTime = wait.peek().requestTime;
+            } else if (!ready.isEmpty()) { //
+                Node poll = ready.poll();
+                waitTime += curTime - poll.requestTime;
                 
-                
-            }
-            
-        
-            curTime++;
+                curTime += poll.processingTime;
+            } else if (ready.isEmpty() && wait.isEmpty())
+                break;
 
         }
         
+        System.out.println("curTime: " + curTime + ", waitTime: " + waitTime);
                           
-        System.out.println("curTime: "+curTime + " , " + waitTime + ", temp : " + temp);
-        return (curTime + waitTime - temp) / jobs.length;
+        return (curTime + waitTime) / jobs.length;
     }
     
     public class Node {
-        int x, y;
-        public Node (int x, int y) {
-            this.x = x ;
-            this.y = y;
+        int requestTime, processingTime;
+        public Node (int requestTime, int processingTime) {
+            this.requestTime = requestTime ;
+            this.processingTime = processingTime;
         }
     }
 
