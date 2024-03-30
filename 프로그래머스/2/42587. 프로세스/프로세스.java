3807c52ba64d18a3dd23d279782f9f464ea76970
이선호 <99793526+preferrrr@users.noreply.github.com>
2024-03-30 17:07:16 +0900

    [level 2] Title: 프로세스, Time: 1.01 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -5,38 +5,27 @@ class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
         
-        LinkedList<Node> queue = new LinkedList();
+        LinkedList<Node> queue = new LinkedList(); //실행 대기 큐, 
         
         for(int i = 0 ; i < priorities.length; i++) {
             Node node = new Node(i, priorities[i]);
             queue.addLast(node);
         }
         
-        int j = 0;
     
         while (true) {
-            Node poll = queue.pollFirst();
+            Node poll = queue.pollFirst(); //큐에서 작업을 뽑고, 
             
-            if(!check(queue, poll.p)) {
+            if(!check(queue, poll.p)) { //이 작업이 가장 높은 우선순위인지 체크한다.
                 queue.addLast(poll);
                 continue;
             }
             
-            j++;
+            answer++;
             
-            if(poll.n == location) {
-                answer = j;
+            if(poll.n == location) { //뽑은 작업이 프로세스의 위치 k와 같으면 반복문을 멈춘다.
                 break;
             }
-            
-
-        
-            
-            
-            for(int i = 0 ; i < queue.size(); i++) {
-                System.out.print(queue.get(i).n + " ");
-            }
-            System.out.println();
         }
         
 
@@ -44,21 +33,13 @@ class Solution {
         return answer;
     }
     
-   static class Node implements Comparable<Node> {
+   static class Node {
         int n, p;
         
         public Node(int n, int p) {
             this.n = n;
             this.p = p;
         }
-        
-        @Override
-        public int compareTo(Node o) {
-            if(o.p == this.p)
-                return o.n - this.n;
-            return o.p - this.p;
-        }
-        
     }
     
     public boolean check(LinkedList<Node> queue, int priority) {
