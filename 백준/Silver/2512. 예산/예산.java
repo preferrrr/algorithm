2afa895d29b9ff0aa0278677be0a0e8c6b2f9a7a
이선호 <99793526+preferrrr@users.noreply.github.com>
2024-05-05 19:04:09 +0900

    [Silver II] Title: 예산, Time: 164 ms, Memory: 15132 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -4,20 +4,24 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m, max = 0;
-    static int[] arr;
-    public static void main(String[] args) throws IOException {
 
+    static int n, m, max;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
         input();
         solve();
     }
 
     static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         n = Integer.parseInt(br.readLine());
         arr = new int[n];
+        max = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         for (int i = 0 ; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             if (arr[i] > max)
@@ -25,31 +29,27 @@ public class Main {
         }
 
         m = Integer.parseInt(br.readLine());
+
     }
 
     static void solve() {
-
         int left = 0;
         int right = max + 1;
 
         while (left < right) {
-
             int mid = (left + right) / 2;
-            int count = 0;
 
-            for (int i = 0 ; i < arr.length; i++) {
-                if (arr[i] > mid) {
-                    count += mid;
-                } else {
-                    count += arr[i];
-                }
+            int sum = 0;
+            for (int i = 0 ; i < n; i++) {
+                if (arr[i] > mid)
+                    sum += mid;
+                else
+                    sum += arr[i];
             }
 
-            if (count <= m) {
+            if (sum <= m)
                 left = mid + 1;
-            } else {
-                right = mid;
-            }
+            else right = mid;
         }
 
         System.out.println(left - 1);
