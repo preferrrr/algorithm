99d91a81340dcfec9f2f34be8faaa782ba9cbfa8
이선호 <99793526+preferrrr@users.noreply.github.com>
2024-05-06 16:56:46 +0900

    [Silver I] Title: 히오스 프로게이머, Time: 264 ms, Memory: 23192 KB -BaekjoonHub

---
+++ 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, k, max;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        max = 0;
+        arr = new int[n];
+
+        for (int i = 0 ; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            if (arr[i] > max)
+                max = arr[i];
+        }
+    }
+
+    static void solve() {
+        long left = 0;
+        long right = max + k + 1;
+
+        while (left < right) {
+            long mid = (left + right) / 2;
+
+            long count = 0;
+            for (int i = 0 ; i < n; i++) {
+                if (arr[i] < mid)
+                    count += mid - arr[i];
+            }
+
+            if (count <= k)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+
+        System.out.println(left - 1);
+    }
+}
